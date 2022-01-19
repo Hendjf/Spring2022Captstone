22f27527b39071330a567dd3650599651dbe9987
Hendjf <70984583+Hendjf@users.noreply.github.com>
2022-01-19 15:45:22 -0500

    DNS scripts

---
+++ CreateNRemoveDNSRecords.ps1
@@ -0,0 +1,6 @@
+﻿ 
+ Add-DnsServerPrimaryZone -ComputerName DC03 -NetworkId "192.168.2.0/24" -ReplicationScope Forest
+ Add-DnsServerResourceRecordA -Name reddeerprint01 -ZoneName corp.ad -IPv4Address 192.168.2.56
+ Add-DnsServerResourceRecordCName -ZoneName corp.ad -HostNameAlias "webapp25.corp.ad" -Name "finance"
+ Remove-DnsServerResourceRecord -ZoneName corp.ad -Name reddeerprint01 -RRType A
+ Get-DnsServerZone -ComputerName DC03
---
+++ DeployDNS.ps1
@@ -0,0 +1,5 @@
+﻿Install-ADDSForest -DomainName companyname.com -DomainNetBIOSName COMPANY -InstallDNS
+Install-WindowsFeature -Name DNS -IncludeManagementTools
+Add-DnsServerPrimaryZone -NetworkID 192.168.64.0/24 -ZoneFile “192.168.64.2.in-addr.arpa.dns”
+Add-DnsServerForwarder -IPAddress 8.8.8.8 -PassThru
+Test-DnsServer -IPAddress 192.168.64.2 -ZoneName "corp.momco.com"
