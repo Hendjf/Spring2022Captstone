ef0623edebe9e68943f9f5db7d3c932bf1151f8d
Hendjf <70984583+Hendjf@users.noreply.github.com>
2022-02-19 15:50:35 -0500

    Update Deploy DNS on local machine

--- DeployDNS.ps1
+++ DeployDNS.ps1
@@ -1,5 +1,5 @@
 ﻿Install-ADDSForest -DomainName companyname.com -DomainNetBIOSName COMPANY -InstallDNS
 Install-WindowsFeature -Name DNS -IncludeManagementTools
-Add-DnsServerPrimaryZone -NetworkID 192.168.64.0/24 -ZoneFile “192.168.64.2.in-addr.arpa.dns”
+Add-DnsServerPrimaryZone -NetworkID 10.1.10.0/24 -ZoneFile “10.1.10.11.in-addr.arpa.dns”
 Add-DnsServerForwarder -IPAddress 8.8.8.8 -PassThru
-Test-DnsServer -IPAddress 192.168.64.2 -ZoneName "corp.momco.com"
+Test-DnsServer -IPAddress 10.1.10.11 -ZoneName "VSRV.AD_DC.local"
